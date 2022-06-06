0c7944805577cf35d44c13721c4ac1f64f8b250b
Konstantin <muselsps@ya.ru>
2022-06-06 21:08:26 +0300

    MVCLessons_2

---
+++ WebCatalog/WebCatalog.Domain/Entity/Catalog.cs
@@ -0,0 +1,24 @@
+﻿namespace WebCatalog.Domain.Entity;
+
+public class Catalog
+{
+    private readonly object _obj = new();
+    public List<Products> ProductsList { get; set; } = new();
+
+    public void AddProduct(Products products)
+    {
+        lock (_obj)
+        {
+            ProductsList.Add(products);
+        }
+    }
+
+    public void RemoveProduct(Products products)
+    {
+        lock (_obj)
+        {
+            ProductsList.Remove(products);
+        }
+    }
+
+}
