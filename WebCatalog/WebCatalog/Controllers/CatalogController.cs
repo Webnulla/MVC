deb2200b19cb9377a5518875b22bb2cefa89cf99
Konstantin <muselsps@ya.ru>
2022-06-09 20:34:26 +0300

    MVCLessons_3

---
+++ WebCatalog/WebCatalog/Controllers/CatalogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCatalog.Domain.Entity;
+
+namespace WebCatalog.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private static Catalog _catalog = new Catalog();
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult Products([FromForm] Products model)
+        {
+            _catalog.AddItem(model);
+            return View(_catalog);
+        }
+
+        [HttpGet]
+        public IActionResult ViewProducts()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveProducts([FromForm] Products model)
+        {
+            _catalog.RemoveItem(model);
+            return View(_catalog);
+        }
+
+        [HttpGet]
+        public IActionResult RemoveProducts()
+        {
+            return View(_catalog);
+        }
+
+
+    }
+}
