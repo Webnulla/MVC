ae7a8937f195482c3b69ab032193779c9892e79d
Konstantin <muselsps@ya.ru>
2022-06-19 11:26:06 +0300

    MVCLessons_6

---
+++ WebCatalog/WebCatalog/Program.cs
@@ -0,0 +1,51 @@
+using Serilog;
+using WebCatalog.Domain.EmailSender;
+using WebCatalog.Domain.Entity;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+Log.Information("Starting up");
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddScoped<Catalog>();
+    builder.Services.AddScoped<EmailService>();
+    builder.Host.UseSerilog((_, conf) => conf.WriteTo.Console());
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Server down");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
