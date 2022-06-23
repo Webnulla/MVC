569a8c7f32d73a648158b1aab0ccd91607a46282
Konstantin <muselsps@ya.ru>
2022-06-23 08:22:25 +0300

    MVCLessons_3_fix

--- WebCatalog/WebCatalog/Controllers/CatalogController.cs
+++ WebCatalog/WebCatalog/Controllers/CatalogController.cs
@@ -1,44 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebCatalog.DAL.Repository.Interfaces;
+using WebCatalog.Domain.EmailSender;
 using WebCatalog.Domain.Entity;
 
 namespace WebCatalog.Controllers
 {
     public class CatalogController : Controller
     {
-        private static Catalog _catalog = new Catalog();
+        private readonly ICatalogRepository _catalogRepository;
+        private readonly EmailService _emailService;
+
+        public CatalogController(EmailService emailService, ICatalogRepository catalogRepository)
+        {
+            _emailService = emailService;
+            _catalogRepository = catalogRepository;
+        }
 
         [HttpGet]
         public IActionResult Products()
         {
-            return View(_catalog);
+            return View();
         }
 
         [HttpPost]
-        public IActionResult Products([FromForm] Products model)
+        public IActionResult Products([FromForm] Products products)
         {
-            _catalog.AddItem(model);
-            return View(_catalog);
+            _catalogRepository.Add(products);
+            return View(products);
         }
 
         [HttpGet]
         public IActionResult ViewProducts()
         {
-            return View(_catalog);
+            var product = _catalogRepository.GetAll();
+            return View(product);
         }
 
         [HttpPost]
-        public IActionResult RemoveProducts([FromForm] Products model)
+        public IActionResult RemoveProducts(int id)
         {
-            _catalog.RemoveItem(model);
-            return View(_catalog);
+            _catalogRepository.Delete(id);
+            return View();
         }
 
         [HttpGet]
         public IActionResult RemoveProducts()
         {
-            return View(_catalog);
+            return View();
         }
 
-
+        public async Task<IActionResult> SendMessage()
+        {
+            await _emailService.SendEmail("muselsps@ya.ru", "New", "Test"); //SocketException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
+            return RedirectToAction("Products");
+        }
     }
 }
