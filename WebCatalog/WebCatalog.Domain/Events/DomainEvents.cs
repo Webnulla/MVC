68079492f2a978d14ff29a600f2bf4a57b93fd39
Konstantin <muselsps@ya.ru>
2022-06-29 19:48:33 +0300

    MVCLessons_8

---
+++ WebCatalog/WebCatalog.Domain/Events/DomainEvents.cs
@@ -0,0 +1,29 @@
+﻿namespace WebCatalog.Domain.Events;
+
+public static class DomainEvents
+{
+    private static readonly Dictionary<Type, List<Delegate>> _handlers = new();
+
+    public static void Register<T>(Action<T> eventHandler)
+        where T : IDomainEvents
+    {
+        if (_handlers.ContainsKey(typeof(T)))
+        {
+            _handlers[typeof(T)].Add(eventHandler);
+        }
+        else
+        {
+            _handlers[typeof(T)] = new List<Delegate>() {eventHandler};
+        }
+    }
+
+    public static void Raise<T>(T domainEvent)
+        where T : IDomainEvents
+    {
+        foreach (Delegate handler in _handlers[domainEvent.GetType()])
+        {
+            var action = (Action<T>)handler;
+            action(domainEvent);
+        }
+    }
+}
